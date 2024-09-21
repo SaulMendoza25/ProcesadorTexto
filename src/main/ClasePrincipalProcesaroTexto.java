c4721df612dc1a335287cc174d1709f17a12884a
Saul Samir Mendoza Moreira <saulmendoza6000@gmail.com>
2024-09-20 22:31:09 -0500

    cambio de nombre clase principal

--- src/main/ClasePrincipalProcesaroTexto.java
+++ src/main/ClasePrincipalProcesaroTexto.java
@@ -2,7 +2,7 @@ package main;
 
 
 
-public class Main {
+public class ClasePrincipalProcesaroTexto {
 
 	public static void main(String[] args) {
 		MarcoProcesadorTexto marcoProcesadorTexto = new MarcoProcesadorTexto(args);
